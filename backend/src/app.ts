11ea4ae0481fedc4e6ce630476bc63de8297b458
lucasdosualdo <lucasdosualdo@gmail.com>
2023-02-01 21:14:30 -0300

    feat: config prisma orm

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -14,17 +13,16 @@ app.get("/status", (req: Request, res: Response) => {
   res.send("ok");
 });
 
-app.get("/googlebook", async (req: Request, res: Response) => {
-  try {
-    const promise = await axios.get(
-      // `https://www.googleapis.com/books/v1/volumes?q=inauthor:machado+de+assis+AND+intitle:dom+casmurro&printType=books&orderBy=newest&key=${API_KEY}`
-      `https://www.googleapis.com/books/v1/volumes?q=intit le:sapiens+uma+breve+histo&printType=books&key=${GOOGLE_API_KEY}`
-    );
-    return res.status(200).send(promise.data);
-  } catch (error) {
-    return res.status(400).send(error);
-  }
-});
+// app.get("/googlebook", async (req: Request, res: Response) => {
+//   try {
+//     const promise = await axios.get(
+//       `https://www.googleapis.com/books/v1/volumes?q=inauthor:machado+de+assis+AND+intitle:dom+casmurro&printType=books&orderBy=newest&key=${GOOGLE_API_KEY}`
+//     );
+//     return res.status(200).send(promise.data);
+//   } catch (error) {
+//     return res.status(400).send(error);
+//   }
+// });
 
 app.listen(process.env.PORT, () => {
   console.log("listening on port " + process.env.PORT);
